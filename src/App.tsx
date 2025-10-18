be5c1a80a6ef6679fb6152f4838767022883b4c1
sara.231.pc <sara.231.pc@gmail.com>
2025-10-18 17:49:50 +0600

    feat: initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{color:"white"}}>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
